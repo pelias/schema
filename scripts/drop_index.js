c08b28d4ba23c3e420675eebfa44bea87f0a23fb
Peter Johnson <wiz@wiz.co.nz>
2014-08-07 11:09:41 -0400

    add --force-yes option to drop index script

--- scripts/drop_index.js
+++ scripts/drop_index.js
@@ -4,13 +4,29 @@ var readline = require('readline'),
     client = require('pelias-esclient')(),
     schema = require('../schema');
 
-rl.question( 'Are you sure you want to drop the pelias index and delete ALL records? ', function( answer ){
-  if( !answer.match(/^y(es)?$/i) ){
-    console.log( 'you must answer \'y\' to confirm. aborting.' )
-    process.exit(0);
-  }
+// use -f or --force-yes to skip the prompt
+if( isForced() ) drop();
+else prompt( drop, fail );
+
+function drop(){
   client.indices.delete( { index: 'pelias' }, function( err, res ){
     console.log( '\n[delete mapping]', '\t', 'pelias', err || '\t', res );
     process.exit( !!err );
   });
-});
+}
+
+function prompt( yes, no ){
+  rl.question( 'Are you sure you want to drop the pelias index and delete ALL records? ', function( answer ){
+    if( !answer.match(/^y(es)?$/i) ) return no();
+    return yes();
+  });
+}
+
+function fail(){
+  console.log( 'you must answer \'y\' to confirm. aborting.' );
+  process.exit(0);
+}
+
+function isForced(){
+  return process.argv.length > 2 && [ '--force-yes', '-f' ].indexOf( process.argv[2] ) > -1;
+}
