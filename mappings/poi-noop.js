203b54262b9670e9728ee8d8570b1c8b1440c463
Peter Johnson <wiz@wiz.co.nz>
2014-08-26 10:04:00 +0100

    deprecated poi-noop

--- mappings/poi-noop.js
+++ mappings/poi-noop.js
@@ -1,17 +1,18 @@
 
-var merge = require('merge');
+// @deprecated
+// var merge = require('merge');
 
-var schema = {
-  'properties': {
-    'name':             merge( {}, require('./partial/hash'), { 'index': 'no' } ),
-    'address':          merge( {}, require('./partial/hash'), { 'index': 'no' } ),
-    'type':             merge( {}, require('./partial/admin'), { 'store': 'no', 'index': 'no' } ),
-    'admin0':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
-    'admin1':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
-    'admin2':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
-    'center_point':     { 'type': 'geo_point', 'index': 'no' },
-    'tags':             merge( {}, require('./partial/hash'), { 'index': 'no' } )
-  }
-}
+// var schema = {
+//   'properties': {
+//     'name':             merge( {}, require('./partial/hash'), { 'index': 'no' } ),
+//     'address':          merge( {}, require('./partial/hash'), { 'index': 'no' } ),
+//     'type':             merge( {}, require('./partial/admin'), { 'store': 'no', 'index': 'no' } ),
+//     'admin0':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
+//     'admin1':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
+//     'admin2':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
+//     'center_point':     { 'type': 'geo_point', 'index': 'no' },
+//     'tags':             merge( {}, require('./partial/hash'), { 'index': 'no' } )
+//   }
+// }
 
-module.exports = schema;
+// module.exports = schema;
