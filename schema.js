6e6a2c23601ca1f909cca34197eb453f3d87da2f
Peter Johnson <insomnia@rcpt.at>
2015-05-12 18:37:55 +0200

    simplify schema to use _default_ mapping

--- schema.js
+++ schema.js
@@ -3,25 +3,7 @@ var doc = require('./mappings/document');
 var schema = {
   settings: require('./settings')(),
   mappings: {
-
-    // openstreetmap points-of-interest
-    osmnode: doc,
-    osmway: doc,
-
-    // geonames
-    geoname: doc,
-
-    // addresses
-    osmaddress: doc,
-    openaddresses: doc,
-
-    // admin boundaries
-    admin0: doc,
-    admin1: doc,
-    admin2: doc,
-    local_admin: doc,
-    locality: doc,
-    neighborhood: doc
+    _default_: doc
   }
 };
 
