849012e46701c613318b08e521e56112ff437074
Peter Johnson <insomnia@rcpt.at>
2015-12-10 17:11:39 +0100

    add integration test

--- integration/run.js
+++ integration/run.js
@@ -13,7 +13,8 @@ var tests = [
   require('./analyzer_peliasZip.js'),
   require('./analyzer_peliasStreet.js'),
   require('./address_matching.js'),
-  require('./source_layer_sourceid_filtering.js')
+  require('./source_layer_sourceid_filtering.js'),
+  require('./bounding_box.js')
 ];
 
 tests.map(function(t) {
