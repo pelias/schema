53ec54f5a19c672a621b3efbbf663a183aa7838f
Peter Johnson <insomnia@rcpt.at>
2016-08-02 16:05:24 +0200

    add script to check mandatory plugins, ensure that this is run when creating an index

---
+++ scripts/cli.js
@@ -0,0 +1,7 @@
+
+var util = require('util');
+
+module.exports.header = function( text ){
+  var rule = new Array( text.length + 3 ).join("-");
+  console.log( util.format("\n\033[0;33m%s\n %s \n%s\033[0m\n", rule, text, rule ) );
+}
