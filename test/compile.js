3fe7a536532f1bfd65c45134d8a23a789825476d
Julian Simioni <julian@simioni.org>
2018-05-18 11:13:03 -0400

    Add commented out code to write expected schema to fixture
    this can help in updating that massive file

--- test/compile.js
+++ test/compile.js
@@ -64,6 +64,10 @@ module.exports.tests.current_schema = function(test, common) {
     // common.diff(schemaCopy, fixture);
     // console.error( JSON.stringify( schemaCopy, null, 2 ) );
 
+    // code to write expected output to the fixture
+    //const fs = require('fs');
+    //fs.writeFileSync(path.resolve( __dirname + '/fixtures/expected.json' ), JSON.stringify(schemaCopy, null, 2));
+
     t.deepEqual(schemaCopy, fixture);
     t.end();
   });
