e39b9e284d1f0a3af8ba668db75584462c560004
Julian Simioni <julian@simioni.org>
2019-01-02 21:18:52 -0800

    feat(create_index): exit with warnings if header size settings are not in effect
    This avoids issues with Elasticsearch 5 headers and Node.js 8.14/10.14
    as described in https://github.com/pelias/schema/pull/339

--- scripts/create_index.js
+++ scripts/create_index.js
@@ -2,6 +2,7 @@ const child_process = require('child_process');
 const http = require('http');
 
 const config = require('pelias-config').generate();
+const logger = require('pelias-logger').get('schema');
 const es = require('elasticsearch');
 
 const cli = require('./cli');
@@ -14,6 +15,20 @@ try {
   child_process.execSync( 'node ' + __dirname + '/check_plugins.js' );
 } catch( e ){
   console.error( "please install mandatory plugins before continuing.\n");
+  process.exit(1);
+}
+
+if (http.maxHeaderSize === undefined) {
+  logger.warning('You are using a version of Node.js that does not support the --max-http-header-size option.' +
+    'You may experience issues when using Elasticsearch 5.' +
+    'See https://github.com/pelias/schema#compatibility for more details.');
+}
+
+if (http.maxHeaderSize < 16384) {
+  logger.error('Max header size is below 16384 bytes. ' +
+    'Be sure to use the provided wrapper script in \'./bin\' rather than calling this script directly.' +
+    'Otherwise, you may experience issues when using Elasticsearch 5.' +
+    'See https://github.com/pelias/schema#compatibility for more details.');
   process.exit(1);
 }
 
