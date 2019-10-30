50ba8d1f257df03556c7bccebe29a7e641a45a09
missinglink <insomnia@rcpt.at>
2019-10-30 12:08:32 +0100

    refactor(scripts): modernize js

--- scripts/create_index.js
+++ scripts/create_index.js
@@ -1,8 +1,5 @@
 const child_process = require('child_process');
-const http = require('http');
-
 const config = require('pelias-config').generate();
-const logger = require('pelias-logger').get('schema');
 const es = require('elasticsearch');
 
 const cli = require('./cli');
@@ -12,20 +9,22 @@ const client = new es.Client(config.esclient);
 
 // check mandatory plugins are installed before continuing
 try {
-  child_process.execSync( 'node ' + __dirname + '/check_plugins.js' );
-} catch( e ){
-  console.error( "please install mandatory plugins before continuing.\n");
+  child_process.execSync(`node ${__dirname}/check_plugins.js`);
+} catch (e) {
+  console.error("please install mandatory plugins before continuing.\n");
   process.exit(1);
 }
 
 cli.header("create index");
-const indexName = config.schema.indexName;
 
-client.indices.create( { index: indexName, body: schema }, function( err, res ){
-  if( err ){
-    console.error( err.message || err, '\n' );
+const indexName = config.schema.indexName;
+const req = { index: indexName, body: schema };
+
+client.indices.create(req, (err, res) => {
+  if (err) {
+    console.error(err.message || err, '\n');
     process.exit(1);
   }
-  console.log( '[put mapping]', '\t', indexName, res, '\n' );
-  process.exit( !!err );
+  console.log('[put mapping]', '\t', indexName, res, '\n');
+  process.exit(!!err);
 });
--- scripts/drop_index.js
+++ scripts/drop_index.js
@@ -1,43 +1,42 @@
-var colors = require('colors/safe');
-var config = require('pelias-config').generate();
-var es = require('elasticsearch');
-var client = new es.Client(config.esclient);
-var readline = require('readline'),
-    rl = readline.createInterface({ input: process.stdin, output: process.stdout }),
-    schema = require('../schema');
+const colors = require('colors/safe');
+const config = require('pelias-config').generate();
+const es = require('elasticsearch');
+const client = new es.Client(config.esclient);
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 // use -f or --force-yes to skip the prompt
-if( isForced() ) drop();
-else prompt( drop, fail );
+if (isForced()) { drop(); }
+else { prompt(drop, fail); }
 
-function drop(){
-  client.indices.delete( { index: config.schema.indexName }, function( err, res ){
-    console.log( '\n[delete mapping]', '\t', config.schema.indexName, err || '\t', res );
-    process.exit( !!err );
+function drop() {
+  const req = { index: config.schema.indexName };
+  client.indices.delete(req, (err, res) => {
+    console.log('\n[delete mapping]', '\t', config.schema.indexName, err || '\t', res);
+    process.exit(!!err);
   });
 }
 
 // check all hosts to see if any is not localhost
 function warnIfNotLocal() {
-  if (config.esclient.hosts.some((env) => { return env.host !== 'localhost'; } )) {
+  if (config.esclient.hosts.some((env) => { return env.host !== 'localhost'; })) {
     console.log(colors.red(`WARNING: DROPPING SCHEMA NOT ON LOCALHOST: ${config.esclient.hosts[0].host}`));
   }
-
 }
 
-function prompt( yes, no ){
+function prompt(yes, no) {
   warnIfNotLocal();
-  rl.question( 'Are you sure you want to drop the ' + config.schema.indexName + ' index and delete ALL records? ', function( answer ){
-    if( !answer.match(/^y(es)?$/i) ) return no();
+  rl.question(`Are you sure you want to drop the ${config.schema.indexName} index and delete ALL records? `, answer => {
+    if (!answer.match(/^y(es)?$/i)) { return no(); }
     return yes();
   });
 }
 
-function fail(){
-  console.log( 'you must answer \'y\' to confirm. aborting.' );
+function fail() {
+  console.log(`you must answer 'y' to confirm. aborting.`);
   process.exit(0);
 }
 
-function isForced(){
-  return process.argv.length > 2 && [ '--force-yes', '-f' ].indexOf( process.argv[2] ) > -1;
+function isForced() {
+  return process.argv.length > 2 && ['--force-yes', '-f'].indexOf(process.argv[2]) > -1;
 }
