d42eae5e76303ebe35cb5faa41b1ed4b8d371b1f
missinglink <insomnia@rcpt.at>
2021-01-12 17:42:44 +1300

    feat(country_code_synonyms): enable country code synonym file for all admin fields

--- integration/admin_abbreviations.js
+++ integration/admin_abbreviations.js
@@ -1,17 +1,17 @@
-/**
- * this test suite ensures that 'admin_abbreviation' fields
- * are indexed in a way that indexing multiple tokens in the
- * same field does not affect scoring even when the tokens
- * are duplicates or prefixes of each other.
-*/
-
 const elastictest = require('elastictest');
 const config = require('pelias-config').generate();
 const getTotalHits = require('./_hits_total_helper');
 
 module.exports.tests = {};
-module.exports.tests.functional = function (test, common) {
-  test('functional', function (t) {
+
+/**
+ * this test ensures that 'admin_abbreviation' fields
+ * are indexed in a way that indexing multiple tokens in the
+ * same field does not affect scoring even when the tokens
+ * are duplicates or prefixes of each other.
+*/
+module.exports.tests.scoring = function (test, common) {
+  test('scoring', function (t) {
 
     var suite = new elastictest.Suite(common.clientOpts, common.create);
     suite.action(done => setTimeout(done, 500)); // wait for es to bring some shards up
@@ -89,6 +89,137 @@ module.exports.tests.functional = function (test, common) {
     });
 
     // search for 'NZ' on 'parent.country_a.ngram' and compare scores
+    suite.assert(done => {
+      suite.client.search({
+        index: suite.props.index,
+        type: config.schema.typeName,
+        body: {
+          query: {
+            match: {
+              'parent.country_a.ngram': {
+                'query': 'nz'
+              }
+            }
+          }
+        }
+      }, (err, res) => {
+        t.equal(err, undefined);
+        t.equal(getTotalHits(res.hits), 2, 'matches both documents');
+        t.equal(res.hits.hits[0]._score, res.hits.hits[1]._score, 'scores match');
+        done();
+      });
+    });
+
+    suite.run(t.end);
+  });
+};
+
+/**
+ * this test ensures that 'admin_abbreviation' fields
+ * include a synonym mapping for country code abbreviations
+ * which maps between alpha2 and alpha3 variants.
+*/
+module.exports.tests.synonyms = function (test, common) {
+  test('synonyms', function (t) {
+
+    var suite = new elastictest.Suite(common.clientOpts, common.create);
+    suite.action(done => setTimeout(done, 500)); // wait for es to bring some shards up
+
+    // index document 1 with country_a='NZL'
+    suite.action(done => {
+      suite.client.index({
+        index: suite.props.index,
+        type: config.schema.typeName,
+        id: '1',
+        body: {
+          parent: {
+            country_a: ['NZL']
+          }
+        }
+      }, done);
+    });
+
+    // index document 2 with country_a='NZ'
+    suite.action(done => {
+      suite.client.index({
+        index: suite.props.index,
+        type: config.schema.typeName,
+        id: '2',
+        body: {
+          parent: {
+            country_a: ['NZ']
+          }
+        }
+      }, done);
+    });
+
+    // search for 'NZL' on 'parent.country_a'
+    suite.assert(done => {
+      suite.client.search({
+        index: suite.props.index,
+        type: config.schema.typeName,
+        body: {
+          query: {
+            match: {
+              'parent.country_a': {
+                'query': 'nzl'
+              }
+            }
+          }
+        }
+      }, (err, res) => {
+        t.equal(err, undefined);
+        t.equal(getTotalHits(res.hits), 2, 'matches both documents');
+        t.equal(res.hits.hits[0]._score, res.hits.hits[1]._score, 'scores match');
+        done();
+      });
+    });
+
+    // search for 'NZ' on 'parent.country_a'
+    suite.assert(done => {
+      suite.client.search({
+        index: suite.props.index,
+        type: config.schema.typeName,
+        body: {
+          query: {
+            match: {
+              'parent.country_a': {
+                'query': 'nz'
+              }
+            }
+          }
+        }
+      }, (err, res) => {
+        t.equal(err, undefined);
+        t.equal(getTotalHits(res.hits), 2, 'matches both documents');
+        t.equal(res.hits.hits[0]._score, res.hits.hits[1]._score, 'scores match');
+        done();
+      });
+    });
+
+    // search for 'NZL' on 'parent.country_a.ngram'
+    suite.assert(done => {
+      suite.client.search({
+        index: suite.props.index,
+        type: config.schema.typeName,
+        body: {
+          query: {
+            match: {
+              'parent.country_a.ngram': {
+                'query': 'nzl'
+              }
+            }
+          }
+        }
+      }, (err, res) => {
+        t.equal(err, undefined);
+        t.equal(getTotalHits(res.hits), 2, 'matches both documents');
+        t.equal(res.hits.hits[0]._score, res.hits.hits[1]._score, 'scores match');
+        done();
+      });
+    });
+
+    // search for 'NZ' on 'parent.country_a.ngram'
     suite.assert(done => {
       suite.client.search({
         index: suite.props.index,
